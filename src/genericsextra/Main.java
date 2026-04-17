570baf77dcf2afd3c897160ef7b8a999acd9f76c
KT147 <kevin.tammm@gmail.com>
2026-04-17 13:49:50 +0300

    .

--- src/genericsextra/Main.java
+++ src/genericsextra/Main.java
@@ -1,75 +1,85 @@
 package genericsextra;
 
+import util.QueryItem;
+import util.QueryList;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+
+record Employee(String name) implements QueryItem{
+
+    @Override
+    public boolean matchFieldValue(String fieldName, String val) {
+        return false;
+    }
+}
 
 public class Main {
     public static void main(String[] args) {
         int studentCount = 10;
         List<Student> students = new ArrayList<>();
-        for(int i = 0; i < studentCount; i++) {
+        for (int i = 0; i < studentCount; i++) {
             students.add(new Student());
         }
         students.add(new LPAStudent());
-        printList(students);
+        printMoreList(students);
 
         List<LPAStudent> lpaStudents = new ArrayList<>();
-        for(int i = 0; i < studentCount; i++) {
+        for (int i = 0; i < studentCount; i++) {
             lpaStudents.add(new LPAStudent());
         }
 
-        printList(lpaStudents);
+        printMoreList(lpaStudents);
+
+        testList(new ArrayList<String>(List.of("Able", "Barry")));
+        testList(new ArrayList<Integer>(List.of(1,2)));
+
+//        var queryList = new QueryList<>(lpaStudents);
+//        var matches = queryList.getMatches("Course", "Python");
+//        printMoreList(matches);
+
+        var students2021 = QueryList.getMatches(students, "YearStarted", "2021");
+        printMoreList(students2021);
+
     }
 
-    public static void printList(List students) {
+//    public static <T extends Student> void printList(List<T> students) {
+//        for (var student : students) {
+//            System.out.println(student.getYearStarted() + ": " + student);
+//        }
+//        System.out.println();
+//    }
+
+    public static void printMoreList(List<? extends Student> students) {
         for (var student : students) {
-            System.out.println(student);
+            System.out.println(student.getYearStarted() + student.getId() + ": " + student);
         }
         System.out.println();
     }
+
+//    public static void testList(List<String> list) {
+//        for (var element : list) {
+//            System.out.println("String: " + element.toUpperCase());
+//        }
+//    }
+//
+//    public static void testList(List<Integer> list) {
+//        for (var element : list) {
+//            System.out.println("Integer: " + element.floatValue());
+//        }
+//    }
+
+    public static void testList(List<?> list) {
+        for (var element : list) {
+            if (element instanceof String s) {
+                System.out.println("String: " + s.toUpperCase());
+            } else if (element instanceof Integer i) {
+                System.out.println("Integer: " + i.floatValue());
+            }
+        }
+    }
+
 }
 
-class Student {
 
-    private String name;
-    private String course;
-    private int yearStarted;
-    protected static Random random = new Random();
-    private static String[] firstNames = {"Ann", "Bill", "Cathy", "John", "Tim"};
-    private static String[] courses = {"C++", "Java", "Python"};
-
-    public Student() {
-        int lastNameIndex = random.nextInt(65, 91);
-        name = firstNames[random.nextInt(5)] + " " + (char) lastNameIndex;
-        course = courses[random.nextInt(3)];
-        yearStarted = random.nextInt(2018, 2023);
-    }
-
-    @Override
-    public String toString() {
-        return "%-15s %-15s %d".formatted(name, course, yearStarted);
-    }
-
-    public int getYearStarted() {
-        return yearStarted;
-    }
-}
-
-class LPAStudent extends Student {
-
-    private double percentComplete;
-
-    public LPAStudent() {
-        percentComplete = random.nextDouble(0, 100.001);
-    }
-
-    @Override
-    public String toString() {
-        return "%s %8.1f%%".formatted(super.toString(), percentComplete);
-    }
-
-    public double getPercentComplete() {
-        return percentComplete;
-    }
-}
